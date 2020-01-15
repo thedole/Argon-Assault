8020ec123845087876504ac0faeeb782519a99d0
Dag Ole Kristoffersen <thedole@gmail.com>
2020-01-15 02:03:06 +0100

    Made enemies explode on particle collisions

---
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    GameObject explosion;
+
+    bool isAlive = true;
+    
+    public void OnParticleCollision(GameObject other)
+    {
+        if (!isAlive)
+        {
+            return;
+        }
+
+        explode();
+    }
+
+    private void explode()
+    {
+        isAlive = false;
+        explosion = Instantiate(explosion, transform.position, transform.rotation);
+        explosion.SetActive(true);
+
+        gameObject.SetActive(false);
+    }
+}
