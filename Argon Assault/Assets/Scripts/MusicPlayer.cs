ffb1d3a828c9609a466e26e651d9100aba6555e3
Dag Ole Kristoffersen <thedole@gmail.com>
2020-01-15 01:01:28 +0100

    Renamed KeepAlive script to musicplayer, made it into a singleton

---
+++ Argon Assault/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        var musicPlayers = FindObjectsOfType<MusicPlayer>();
+        if (musicPlayers.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        Object.DontDestroyOnLoad(gameObject);
+    }
+}
